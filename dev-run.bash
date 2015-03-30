d57ad021bb2fc21975553a6c2a5da4c5e4d7ba16
BenWhitehead <ben.whitehead@mesosphere.io>
2015-03-30 12:05:33 -0700

    Fix a few paths in the dev-run script

--- dev-run.bash
+++ dev-run.bash
@@ -11,7 +11,7 @@ function main {(
     export CASSANDRA_ZK=zk://localhost:2181/cassandra-mesos
     export EXECUTOR_FILE_PATH=$(ls $(pwd)/cassandra-dist/target/tarball/cassandra-executor.jar)
     export JRE_FILE_PATH=$(pwd)/cassandra-dist/target/tarball/jre.tar.gz
-    export CASSANDRA_FILE_PATH=$(pwd)/cassandra-dist/target/tarball/apache-cassandra-*.tar.gz
+    export CASSANDRA_FILE_PATH=$(ls $(pwd)/cassandra-dist/target/tarball/apache-cassandra-*.tar.gz)
     export CASSANDRA_NODE_COUNT=3
     export CASSANDRA_RESOURCE_CPU_CORES=1
     export CASSANDRA_RESOURCE_MEM_MB=512
@@ -19,7 +19,7 @@ function main {(
     export CASSANDRA_HEALTH_CHECK_INTERVAL_SECONDS=60
     export CASSANDRA_ZK_TIMEOUT_MS=10000
 
-    java -cp $(pwd)/cassandra-dist/target/cassandra-framework.jar io.mesosphere.mesos.frameworks.cassandra.Main
+    java -cp $(pwd)/cassandra-dist/target/tarball/cassandra-framework.jar io.mesosphere.mesos.frameworks.cassandra.Main
 
 )}
 

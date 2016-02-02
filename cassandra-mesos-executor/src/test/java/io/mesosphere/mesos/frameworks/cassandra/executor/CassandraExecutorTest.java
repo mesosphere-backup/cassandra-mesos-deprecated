0fbfdc2094007245ca5420d007858ad15818fdaa
Elise Huard <elise@jabberwocky.eu>
2016-02-02 14:20:24 +0000

    changes due to update

--- cassandra-mesos-executor/src/test/java/io/mesosphere/mesos/frameworks/cassandra/executor/CassandraExecutorTest.java
+++ cassandra-mesos-executor/src/test/java/io/mesosphere/mesos/frameworks/cassandra/executor/CassandraExecutorTest.java
@@ -18,42 +18,246 @@ package io.mesosphere.mesos.frameworks.cassandra.executor;
 import com.google.common.base.Predicate;
 import com.google.common.collect.FluentIterable;
 import io.mesosphere.mesos.frameworks.cassandra.CassandraFrameworkProtos;
+import io.mesosphere.mesos.frameworks.cassandra.executor.jmx.JmxConnect;
 import org.apache.cassandra.service.ActiveRepairService;
+import org.apache.cassandra.service.StorageServiceMBean;
+import org.apache.cassandra.utils.progress.ProgressEvent;
+import org.apache.cassandra.utils.progress.ProgressEventType;
+import org.apache.cassandra.utils.progress.jmx.JMXProgressSupport;
+import org.apache.cassandra.locator.EndpointSnitchInfoMBean;
+import org.apache.mesos.ExecutorDriver;
 import org.apache.mesos.Protos;
 import org.jetbrains.annotations.NotNull;
 import org.joda.time.Duration;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.slf4j.Marker;
 
+import javax.management.ListenerNotFoundException;
+import javax.management.NotificationBroadcasterSupport;
+import javax.management.NotificationListener;
+import javax.management.NotificationFilter;
+import java.lang.management.ManagementFactory;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static com.google.common.collect.FluentIterable.from;
 import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+import static org.mockito.Mockito.*;
 
 public class CassandraExecutorTest {
-    @NotNull
-    private final CassandraExecutor executor;
-    @NotNull
-    private final MockExecutorDriver driver;
-    @NotNull
-    private final TestObjectFactory objectFactory;
+
+    /**
+     * UUT
+     */
+    private CassandraExecutor executor;
+
+    // TODO: This really should be mocked with mockito, but since the code already exists I
+    // didn't see a reason to replace it for now
+    private MockExecutorDriver driver;
+
+    @Mock
+    private ObjectFactory objectFactory;
+
+    @Mock
+    private JmxConnect jmxConnect;
+
+    @Mock
+    private StorageServiceMBean storageServiceProxy;
+
+    // Counts the # of listeners for the storageServiceProxy
+    private AtomicInteger listenerCount = new AtomicInteger();
+    private NotificationBroadcasterSupport notificationBroadcaster = new NotificationBroadcasterSupport();
+    private JMXProgressSupport progressEmitter = new JMXProgressSupport(notificationBroadcaster);
+
+    private SequenceAnswer<Integer> repairCommandSeq = new SequenceAnswer<Integer>(1, x -> x + 1);
+
+    @Mock
+    private EndpointSnitchInfoMBean endpointSnitchInfoProxy;
+
     @NotNull
     private final Protos.TaskID taskIdExecutor;
+
     @NotNull
     private final Protos.TaskID taskIdMetadata;
+
     @NotNull
     private final Protos.TaskID taskIdServer;
 
     /**
-     * Every JUnit tests is ran in a new instance of this class, therefore we can initialize everything in the constructor
+     * Every JUnit tests is ran in a new instance of this class, therefore we can initialize some constant things in this constructor
      */
     public CassandraExecutorTest() {
-        objectFactory = new TestObjectFactory();
-        executor = new CassandraExecutor(objectFactory);
-        driver = new MockExecutorDriver(executor, Protos.ExecutorID.newBuilder().setValue("executor").build());
         taskIdExecutor = Protos.TaskID.newBuilder().setValue("executor").build();
         taskIdMetadata = Protos.TaskID.newBuilder().setValue("executor.metadata").build();
         taskIdServer = Protos.TaskID.newBuilder().setValue("executor.server").build();
+    }
+
+    /**
+     * Functional interface to mockito answers allowing easy and
+     * readable implementation of returning a determinable sequence
+     * of things
+     */
+    private static class SequenceAnswer<T> implements Answer<T> {
+        private T runner;
+        private Function<T, T> nextFunction;
+
+        /**
+         * Constructs a new SequenceAnswer with a given initial value and
+         * a mapping function
+         */
+        public SequenceAnswer(T firstValue, Function<T, T> nextFunction) {
+            this.runner = firstValue;
+            this.nextFunction = nextFunction;
+        }
+
+        /**
+         * Gets the current runner value
+         */
+        public T getRunner() {
+            return runner;
+        }
+
+        @Override
+        public T answer(InvocationOnMock invocation) {
+            T temp = runner;
+            runner = nextFunction.apply(runner);
+            return temp;
+        }
+    }
+
+    /**
+     * Variadic version of java.util.Arrays.stream
+     *
+     * @see java.util.Arrays.stream
+     */
+    private static <T> Stream<T> arrayStream(T... a) {
+        return Arrays.stream(a);
+    }
+
+    /**
+     * Convenience function for emitting repair notifications
+     */
+    private void emitRepairNotification(ProgressEvent evt) {
+        String tag = "repair:" + (repairCommandSeq.getRunner() - 1);
+        progressEmitter.progress(tag, evt);
+    }
+
+    @Before
+    public void initMocks() {
+        MockitoAnnotations.initMocks(this);
+        executor = new CassandraExecutor(objectFactory);
+        driver = new MockExecutorDriver(executor, Protos.ExecutorID.newBuilder().setValue("executor").build());
+
+        // Used in custom counter answers
+        StorageServiceMBean storageServiceProxy2 = mock(StorageServiceMBean.class);
+
+        try {
+            when(storageServiceProxy.getOperationMode())
+                .thenReturn("NORMAL");
+            when(storageServiceProxy.isGossipRunning())
+                .thenReturn(true);
+            when(storageServiceProxy.isJoined())
+                .thenReturn(true);
+            when(storageServiceProxy.isNativeTransportRunning())
+                .thenReturn(true);
+            when(storageServiceProxy.isRPCServerRunning())
+                .thenReturn(true);
+            when(storageServiceProxy.isInitialized())
+                .thenReturn(true);
+            when(storageServiceProxy.getReleaseVersion())
+                .thenReturn("5.6.7");
+            when(storageServiceProxy.getClusterName())
+                .thenReturn("mocked-unit-test");
+            when(storageServiceProxy.getTokens())
+                .thenReturn(Arrays.asList("1", "2"));
+            when(storageServiceProxy.getTokens(anyString()))
+                .thenReturn(Arrays.asList("1", "2"));
+            when(storageServiceProxy.getTokenToEndpointMap())
+                .thenReturn(Collections.singletonMap("1", "1.2.3.4"));
+            when(storageServiceProxy.getLocalHostId())
+                .thenReturn(UUID.randomUUID().toString());
+            List<String> keyspaces = Arrays.asList("system", "foo", "bar", "baz");
+            when(storageServiceProxy.getKeyspaces())
+                .thenReturn(keyspaces);
+            when(storageServiceProxy.forceRepairAsync(anyString(), anyBoolean(), anyBoolean(), anyBoolean(), anyBoolean(), anyVararg()))
+                .thenAnswer(repairCommandSeq);
+
+            // Implement a counter on the # of listeners
+            doAnswer(new Answer<Object>() {
+                @Override
+                public Object answer(InvocationOnMock invocation) {
+                    NotificationListener listener = invocation.getArgumentAt(0, NotificationListener.class);
+                    NotificationFilter filter = invocation.getArgumentAt(1, NotificationFilter.class);
+                    Object o = invocation.getArgumentAt(2, Object.class);
+                    notificationBroadcaster.addNotificationListener(listener, filter, o);
+                    listenerCount.getAndIncrement();
+                    return null;
+                }
+            }).when(storageServiceProxy).addNotificationListener(any(NotificationListener.class), any(NotificationFilter.class), anyObject());
+            Answer<Object> dAnswer = new Answer<Object>() {
+                @Override
+                public Object answer(InvocationOnMock invocation) {
+                    NotificationListener listener = invocation.getArgumentAt(0, NotificationListener.class);
+                    try {
+                        notificationBroadcaster.removeNotificationListener(listener);
+                    } catch (ListenerNotFoundException e) {
+                        throw new RuntimeException(e);
+                    }
+                    listenerCount.getAndDecrement();
+                    return null;
+                }
+            };
+            doAnswer(dAnswer)
+                .when(storageServiceProxy)
+                .removeNotificationListener(any(NotificationListener.class));
+            doAnswer(dAnswer)
+                .when(storageServiceProxy)
+                .removeNotificationListener(any(NotificationListener.class), any(NotificationFilter.class), anyObject());
+
+
+            when(endpointSnitchInfoProxy.getRack(anyString()))
+                .thenReturn("rack");
+            when(endpointSnitchInfoProxy.getDatacenter(anyString()))
+                .thenReturn("datacenter"); 
+            when(endpointSnitchInfoProxy.getSnitchName())
+                .thenReturn("mock-snitch");
+
+            when(jmxConnect.getRuntimeProxy())
+                .thenReturn(ManagementFactory.getRuntimeMXBean());
+            when(jmxConnect.getStorageServiceProxy())
+                .thenReturn(storageServiceProxy);
+            when(jmxConnect.getEndpointSnitchInfoProxy())
+                .thenReturn(endpointSnitchInfoProxy);
+            for (String keyspace : keyspaces) {
+                when(jmxConnect.getColumnFamilyNames(keyspace))
+                    .thenReturn(arrayStream("a", "b", "c")
+                            .map(cf -> keyspace + "_" + cf)
+                            .collect(Collectors.toList()));
+            }
+
+            when(objectFactory.newJmxConnect((CassandraFrameworkProtos.JmxConnect)notNull()))
+                .thenReturn(jmxConnect);
+            when(objectFactory.launchCassandraNodeTask((Marker)notNull(), (CassandraFrameworkProtos.CassandraServerRunTask)notNull()))
+                .thenReturn(new MockWrappedProcess(42))
+                .thenReturn(new MockWrappedProcess(43));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
@@ -116,7 +320,7 @@ public class CassandraExecutorTest {
         final CassandraFrameworkProtos.ClusterJobType jobType = CassandraFrameworkProtos.ClusterJobType.CLEANUP;
 
         assertNull(executor.getCurrentJob());
-        assertTrue(objectFactory.storageServiceProxy.listeners.isEmpty());
+        assertEquals(0, listenerCount.get());
 
         final Protos.TaskID taskId = Protos.TaskID.newBuilder().setValue(driver.executorInfo.getExecutorId().getValue() + '.' + jobType).build();
         driver.launchTask(
@@ -159,16 +363,17 @@ public class CassandraExecutorTest {
         assertEquals(1, taskStatusList.size());
         assertEquals(taskId, taskStatusList.get(0).getTaskId());
         assertEquals(Protos.TaskState.TASK_FINISHED, taskStatusList.get(0).getState());
+        assertEquals(0, driver.taskStatusList().size());
 
         assertNull(executor.getCurrentJob());
-        assertTrue(objectFactory.storageServiceProxy.listeners.isEmpty());
+        assertEquals(0, listenerCount.get());
     }
 
     private void repairJob() {
         final CassandraFrameworkProtos.ClusterJobType jobType = CassandraFrameworkProtos.ClusterJobType.REPAIR;
 
         assertNull(executor.getCurrentJob());
-        assertTrue(objectFactory.storageServiceProxy.listeners.isEmpty());
+        assertEquals(0, listenerCount.get());
 
         final Protos.TaskID taskId = Protos.TaskID.newBuilder().setValue(driver.executorInfo.getExecutorId().getValue() + '.' + jobType).build();
         driver.launchTask(
@@ -197,28 +402,37 @@ public class CassandraExecutorTest {
             });
         assertEquals(1, nodeJobStatusMessages.size());
         assertTrue(nodeJobStatusMessages.get(0).hasNodeJobStatus());
-        assertTrue(driver.taskStatusList().isEmpty());
+        assertEquals(0, driver.taskStatusList().size());
 
         assertNotNull(executor.getCurrentJob());
         // MBean emits system KS + 3 other KS; one KS is currently processing; 2 KS remaining
         assertEquals(2, executor.getCurrentJob().getRemainingKeyspaces().size());
         assertFalse(executor.getCurrentJob().isFinished());
-        assertFalse(objectFactory.storageServiceProxy.listeners.isEmpty());
+        assertTrue(listenerCount.get() > 0);
 
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.STARTED);
-        // SESSION_SUCCESS is called for each repaired range (one KS/CF repair usually contains a lot of ranges)
-        // Just simulate that here.
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.SESSION_SUCCESS);
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.SESSION_SUCCESS);
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.SESSION_SUCCESS);
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.FINISHED);
+        emitRepairNotification(new ProgressEvent(ProgressEventType.START, 0, 3));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.PROGRESS, 1, 3));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.PROGRESS, 2, 3));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.PROGRESS, 3, 3));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.SUCCESS, 3, 3));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.COMPLETE, 3, 3));
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.STARTED);
+        //// SESSION_SUCCESS is called for each repaired range (one KS/CF repair usually contains a lot of ranges)
+        //// Just simulate that here.
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.SESSION_SUCCESS);
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.SESSION_SUCCESS);
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.SESSION_SUCCESS);
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.FINISHED);
 
         assertEquals(1, executor.getCurrentJob().getRemainingKeyspaces().size());
         assertEquals(1, executor.getCurrentJob().getKeyspaceStatus().size());
         assertFalse(executor.getCurrentJob().isFinished());
 
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.STARTED);
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.FINISHED);
+        emitRepairNotification(new ProgressEvent(ProgressEventType.START, 0, 0));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.SUCCESS, 0, 0));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.COMPLETE, 0, 0));
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.STARTED);
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.FINISHED);
 
         assertEquals(0, executor.getCurrentJob().getRemainingKeyspaces().size());
         assertEquals(2, executor.getCurrentJob().getKeyspaceStatus().size());
@@ -232,13 +446,16 @@ public class CassandraExecutorTest {
         assertTrue(messages.get(0).hasNodeJobStatus());
         assertTrue(driver.taskStatusList().isEmpty());
 
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.STARTED);
-        objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.FINISHED);
+        emitRepairNotification(new ProgressEvent(ProgressEventType.START, 0, 0));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.SUCCESS, 0, 0));
+        emitRepairNotification(new ProgressEvent(ProgressEventType.COMPLETE, 0, 0));
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.STARTED);
+        //objectFactory.storageServiceProxy.emitRepairNotification(ActiveRepairService.Status.FINISHED);
 
         assertEquals(0, executor.getCurrentJob().getRemainingKeyspaces().size());
         assertEquals(3, executor.getCurrentJob().getKeyspaceStatus().size());
         assertTrue(executor.getCurrentJob().isFinished());
-        assertTrue(objectFactory.storageServiceProxy.listeners.isEmpty());
+        assertEquals(0, listenerCount.get());
 
         driver.frameworkMessage(CassandraFrameworkProtos.TaskDetails.newBuilder()
                 .setType(CassandraFrameworkProtos.TaskDetails.TaskDetailsType.NODE_JOB_STATUS)
@@ -252,7 +469,7 @@ public class CassandraExecutorTest {
         assertEquals(Protos.TaskState.TASK_FINISHED, taskStatusList.get(0).getState());
 
         assertNull(executor.getCurrentJob());
-        assertTrue(objectFactory.storageServiceProxy.listeners.isEmpty());
+        assertEquals(0, listenerCount.get());
     }
 
     private void startServer() throws com.google.protobuf.InvalidProtocolBufferException, InterruptedException {
